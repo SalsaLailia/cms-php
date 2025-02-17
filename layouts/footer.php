4a65329790e9f02fc4580db121847b7e2f41a499
SalsaLailia <salsalailiaputri@gmail.com>
2025-02-17 02:35:48 -0800

    mengisi header footer navbar

---
+++ layouts/footer.php
@@ -0,0 +1,37 @@
+<section class="bg-body-secondary  py-5 mt-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-4">
+              <h3>Follow</h3>
+              <p>Kalian bisa menemukan saya disini</p>
+            <a href="https://www.instagram.com/salsaliaptry/">
+              <i class="fs-1 bi bi-instagram"></i>
+            </a>
+            <a href="https://www.tiktok.com/@salsaliaptr?_t=ZS-8textYreksr&_r=1">
+              <i class="fs-1 bi bi-tiktok"></i>
+            </a>
+            <a href="https://www.youtube.com/@salsalailia9134">
+              <i class="fs-1 bi bi-youtube"></i>
+            </a>
+
+            </div>
+            <div class="col-4">
+              <h3>About</h3>
+              <p>hhhhhhh</p>
+            </div>
+            <div class="col-4">
+              <h3>Contact</h3>
+              <p>Phone: 0857-0050-8859</p><br>
+              <p>Email: salsalailia@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+      <footer class="bg-primary py-2">
+        <p class="text-center text-white">Copyright salsa lailia@2025</p>
+
+       </footer>
+    
+</body>
+</html>
