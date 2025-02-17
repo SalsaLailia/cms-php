e0ebeedab676cc20a3d6672efd31f62ba62650b5
SalsaLailia <salsalailiaputri@gmail.com>
2025-02-17 02:50:31 -0800

    index switch

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 include "layouts/header.php";
 include "layouts/navbar.php";
-include "layouts/ooter.php";
+include "layouts/footer.php";
+
+// if (isset($_GET['page'])){
+//     $page = $_GET
+// }
+
+switch ($page) {
+    case 'home':
+        include "view/home.php";
+        break;
+    default:
+        include "view/home.php";
+        break;
+}
+include "layout/footer.php";
 ?>
