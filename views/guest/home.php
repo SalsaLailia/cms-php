15e57a1fb4541120425d0ace4cc3ebfcbb32d59f
SalsaLailia <salsalailiaputri@gmail.com>
2025-02-17 02:52:15 -0800

    home

---
+++ views/guest/home.php
@@ -0,0 +1,44 @@
+<section class="text-bg-primary p-5">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-2">
+                <div class="col-6 col-sm-12 p-5">
+                  <h1>Halo, saya Salsalailia</h1>
+                  <p class="fs-5">Saya berusia 18 tahun dan saat ini sedang bersekolah di SMK 2 Yogyakarta. Ayo mengenal saya lebih jauh, bisa klik disini yaa!</p>
+                  <button type="button" class="btn btn-outline-light">About Us</button>
+                </div>
+                <div class="col-6 col-sm-12">
+                  <img class="img-fluid" src="foto.web__1.png" alt="">
+                </div>
+            </div>
+            <!-- grid sistem -->
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row text-center mt-3">
+          <div class="col">
+            <h1 class="display-1">Portofolio</h1>
+            <p class="fs-5 border-bottom border-2 border-primary">Portofolio yang sudah pernah saya buat selama saya belajar di sekolah</p>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 mt-5">
+          <div class="col-6 col-sm-12">
+            <img class="img-fluid" src="apk porto.png" alt="">
+          </div>
+          <div class="col-sm-12">
+            <div class="row mb-5">
+              <h3><i class="fs-2 bi bi-badge-3d"></i> Modeling atau 3D sculpting</h3>
+              <p>Pembuatan konten 3D, termasuk modeling, animasi, dan rendering.</p>
+            </div>
+            <div class="row mb-5">
+              <h3><i class="fs-2 bi bi-globe-asia-australia"></i> Panorama 3D atau visualisasi 3D</h3>
+              <p>Pembuatan panorama 3D atau visualisasi 3D dari sebuah tempat atau lingkungan. Ini sering digunakan dalam pembuatan tur virtual, aplikasi arsitektur, atau bahkan di video game.</p>
+            </div>
+            <div class="row mb-5">
+              <h3><i class="fs-2 bi bi-file-earmark-image"></i> Desain dan pengeditan</h3>
+              <p>Menciptakan atau memodifikasi gambar, video, teks, dan elemen grafis lainnya</p>
+            </div>
+          </div>
+        </div>
+      </div>
