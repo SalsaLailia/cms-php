ee41a1c721d28af8d60865072a09c79a605c4e8e
SalsaLailia <salsalailiaputri@gmail.com>
2025-02-17 03:35:15 -0800

    guest aboutus portofolio

---
+++ views/guest/portofolio-detail.php
@@ -0,0 +1,74 @@
+<section>
+        <div class="row">
+            <img class="img-fluid" src="portofolio.3dvista.png" alt="">
+        </div>
+        <div class="container mt-5">
+           <div class="row Athour">
+             <div class="col-9 fs-5 text-secondary mx-auto">
+              <p>Athour: Salsalailia | 5 Febuari 2025</p>
+          </div>
+          <div class="row">
+            <div class="col-9 fs-5 mx-auto">
+                <h2>Membuat Virtual Reality dengan 3D Vista</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-9 fs-5 mx-auto">
+              <p>Keberhasilan saya dalam proyek Virtual Reality Gedung Rumah Sakit Akademik UGM. Dengan menggunakan aplikasi 3D Vista dan kamera 360 derajat, aplikasi ini memberikan panduan visual interaktif untuk menjelajahi gedung secara virtual, meningkatkan kenyamanan dan efisiensi dalam navigasi. Proyek ini memanfaatkan teknologi untuk mendukung pelayanan kesehatan dan pendidikan.</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-9 fs-5 mx-auto">
+              <h4>Fitur Utama:</h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-9 fs-5 mx-auto">
+              <ul style="list-style: disc;">
+                <li>Pembuatan Tur Virtual Interaktif: Memungkinkan pembuatan tur virtual 360 derajat yang interaktif, di mana pengguna dapat menjelajahi ruang atau area secara bebas.</li>
+                <li>Integrasi Kamera 360 Derajat: Mendukung penggunaan gambar dan video 360 derajat untuk menciptakan pengalaman imersif bagi pengguna.</li>
+                <li>Navigasi yang Mudah: Memungkinkan navigasi yang mulus antar ruang, dengan opsi untuk zoom in, zoom out, dan beralih antar lokasi.</li>
+                <li>Desain Responsif: Dapat diakses melalui berbagai perangkat, termasuk desktop, tablet, dan smartphone, dengan tampilan yang disesuaikan.</li>
+                <li>Personalisasi Tampilan: Memungkinkan penyesuaian elemen visual dan tampilan untuk mencocokkan desain atau branding spesifik.</li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-9 fs-5 mx-auto">
+              <p>Proyek Virtual Reality Gedung Rumah Sakit Akademik UGM mempermudah pasien dan mahasiswa koas dalam menemukan ruangan yang mereka cari. Dengan teknologi VR, pengguna dapat menjelajahi gedung secara interaktif, mengurangi kebingungan, dan meningkatkan efisiensi dalam navigasi, serta membantu mahasiswa koas beradaptasi dengan lingkungan rumah sakit untuk pembelajaran.</p>
+            </div>
+          </div>
+       </div>
+      </section>
+            <!-- grid sistem -->
+      </div>
+     <section>
+      <div class="container">
+        <div class="row text-center mt-3">
+          <div class="col fs-2">
+            <p>Portofolios</p>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 mt-5">
+        <div class="col">
+          <img src="https://dummyimage.com/600x400" alt class="img-fluid">
+          <p class="mt-3">11 Febuari 2025</p>
+          <h3 class="mt-4">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+        <div class="col">
+          <img src="https://dummyimage.com/600x400" alt class="img-fluid">
+          <p class="mt-3">11 Febuari 2025</p>
+          <h3 class="mt-4">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+        <div class="col">
+          <img src="https://dummyimage.com/600x400" alt class="img-fluid">
+          <p class="mt-3">11 Febuari 2025</p>
+          <h3 class="mt-4">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div class="row my-5">
+        <div class="col-4 mx-auto text-center">
+          <a href="#" class="btn btn-lg btn-outline-primary">More Portofolio</a>
+        </div>
+      </div>
+     </section>
