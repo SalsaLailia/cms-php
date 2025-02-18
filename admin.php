3d9679eeed78e33c43c3b35d44c6631c2e115f8c
SalsaLailia <salsalailiaputri@gmail.com>
2025-02-18 03:41:30 -0800

    layouts admin

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+include "layouts/admin/head.php";
+include "layouts/admin/sidebar.php";
+
+switch($page){
+    case "dashboard":
+        break;
+    case "portofolio-table":
+        break;
+}
+
+include "layouts/admin/footer.php";
+?>
